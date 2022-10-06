8dbffae2662d1dfe99e3713b1a6158268275d45c
njohal13 <navneet_johal@hotmail.com>
2022-10-05 20:49:29 -0700

    added delete Task in App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,50 @@ import Header from './components/Header'
 import Tasks from './components/Tasks';
 import { useState } from "react"
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctor's Appointment",
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: 'Feb 10th at 12:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Grocery Shopping",
+      day: 'Feb 5th at 5:30pm',
+      reminder: true,
+    },
+  ])
+
+
+
+//Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task)=> task.id !== id))
+}
+
+//Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+}
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> ): (
+        'No Tasks To Show'
+        )}
       
       
     </div>
   );
 }
+
 
 export default App;
